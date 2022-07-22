3b971974c0f6dcdd22f4b8a32a7890635be0729f
priyamohan08 <109510877+priyamohan08@users.noreply.github.com>
2022-07-22 14:57:27 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,37 @@
+# a=6
+# number=8
+# _=9
+# print(_)
+# print(a)
+# print(number)
+# print(h) # NameError: name 'h' is not defined
+
+# _com5=7
+# rt=3
+# 6t=7 # SyntaxError
+# a b=5 # SyntaxError
+# c_r=7 
+# abcdefghijklnmnopqrstvwxyz=4
+
+# A=5 ; B=7 
+# print(A)
+# print(B)
+# C=A
+# A=B
+# B=C
+# print(A)
+# print(B)
+# print(C)
+
+# x=6 ; y=8
+# print(x) ; print(y)
+# x,y=y,x
+# print(x) ; print(y)
+
+# print('abc'.isidentifier())
+# print('a b c'.isidentifier())
+# print('_'.isidentifier())
+# print('a_b_'.isidentifier())
+# print('a3b_'.isidentifier())
+# print('a3b_7'.isidentifier())
+# print('43b_7'.isidentifier())
